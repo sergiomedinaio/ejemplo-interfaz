d9e9005f5196e1329d1189631f67b528b34c9184
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-11-07 15:07:06 -0300

    chore: add initial setup

---
+++ src/Curso.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class Curso {
+    public ArrayList<Estudiante> estudiantes;
+}
---
+++ src/IConLista.java
@@ -0,0 +1,5 @@
+public interface IConLista {
+    void add(Estudiante estudiante);
+    void remove(Estudiante estudiante);
+    void find(int numeroMatricula);
+}
