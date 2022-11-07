7d175c391faa97fd415cf2e2f85589326acde87a
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-11-07 15:10:09 -0300

    feat: add list management support

--- src/Curso.java
+++ src/Curso.java
@@ -1,5 +1,20 @@
 import java.util.ArrayList;
 
-public class Curso {
+public class Curso implements IConLista{
     public ArrayList<Estudiante> estudiantes;
+
+    @Override
+    public void add(Estudiante estudiante) {
+        this.estudiantes.add(estudiante);
+    }
+
+    @Override
+    public void remove(Estudiante estudiante) {
+        this.remove(estudiante);
+    }
+
+    @Override
+    public void find(int numeroMatricula) {
+        return;
+    }
 }
